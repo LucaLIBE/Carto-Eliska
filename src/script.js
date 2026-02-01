e1a34dee1586f4441577338c5c53fca7078ef41a
LucaLIBE <lluca.libe@gmail.com>
2026-02-01 15:04:53 +0100

    v0.1

--- src/script.js
+++ src/script.js
@@ -1,25 +1,48 @@
+import {addGeojson} from './utils.js'
+
+const colorLuca = "rgb(0,0,255)";
+const colorEliska = "rgb(255, 0, 234)";
+
+const config = {"borderColor":"#555", "weight":1, "fillColor":"#ffffff", "fillOpacity":"0.2"}
 // Init map
-const map = L.map('map').setView([20,0],2);
+const map = L.map('map',{renderer: L.canvas()}).setView([20,0],2);
+const background = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '© OpenStreetMap'}).addTo(map);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '© OpenStreetMap'}).addTo(map);
+const baseMaps = {
+  "OpenStreetMap": background
+};
 
-//Charger le GeoJSON des pays
-fetch('data/CNTR_RG_60M_2024_3035.geojson')
+const memoriesLayer = L.layerGroup();
+
+L.marker([48.8566, 2.3522])
+    .bindPopup('<strong>Souvenir</strong><br>À Paris.')
+    .addTo(memoriesLayer);
+
+memoriesLayer.addTo(map);
+
+const overlays = {
+  "Memories" : memoriesLayer,
+}
+
+fetch('data/countries.json')
   .then(response => response.json())
-  .then(data => {
-    //Ajouter la couche GeoJSON
-    const countriesLayer = L.geoJSON(data, {
-      style: function (feature) {
-        return {
-          color: '#555',
-          weight: 1,
-          fillColor: '#cce5ff',
-          fillOpacity: 0.6
-        };
-      },
-      onEachFeature: function (feature, layer) {
-        const name = feature.properties.name || 'Pays';
-        layer.bindPopup('<strong>' + name + '</strong>');
-      }
-    }).addTo(map);
+  .then(countriesData => {
+    //Charger le GeoJSON des pays
+    fetch('data/CNTR_RG_60M_2024_4326.geojson')
+      .then(response => response.json())
+      .then(data => {
+        //Ajouter la layer GeoJSON Luca
+        const [countriesLayerLuca, countriesLucaNew] = addGeojson(data, config, countriesData.countriesLuca, colorLuca);
+        layersControl.addOverlay(countriesLayerLuca, 'Countries Luca');
+
+        //Ajouter la layer GeoJSON Eliska
+        const [countriesLayerEliska, countriesEliskaNew] = addGeojson(data, config, countriesData.countriesEliska, colorEliska);
+        layersControl.addOverlay(countriesLayerEliska, 'Countries Eliska');
+      });
   })
+
+const layersControl = L.control.layers(baseMaps, overlays).addTo(map);
+/*
+window.onbeforeunload = function () {
+    return "Do you really want to close?";
+};*/
---
+++ src/utils.js
@@ -0,0 +1,48 @@
+export function addGeojson(data, config, countriesData, color) {
+    var countriesDataNew = countriesData
+    var personLayer = L.geoJSON(data, {
+          style: function (feature) {
+            const isCountryInList=countriesData.includes(feature.properties.CNTR_ID);
+            return {
+              color: config.borderColor,
+              weight: config.weight,
+              fillColor: isCountryInList? color : config.fillColor,
+              fillOpacity: config.fillOpacity
+            }; 
+          },
+          onEachFeature: function (feature, layer) {
+            layer.on('click', function(e){
+                if (countriesData.includes(feature.properties.CNTR_ID)) {
+                    console.log('oh')
+                    layer.setStyle({
+                    color: config.borderColor,
+                    weight: config.weight,
+                    fillColor: config.fillColor,
+                    fillOpacity: config.fillOpacity
+                    });
+                countriesDataNew = removeItem(countriesDataNew, feature.properties.CNTR_ID)
+                }
+                else {
+                    console.log('wow')
+                    layer.setStyle({
+                        color: config.borderColor,
+                        weight: config.weight,
+                        fillColor: color,
+                        fillOpacity: config.fillOpacity
+                    });
+                    countriesDataNew.push(feature.properties.CNTR_ID)
+                }
+            })
+      }
+        });
+    return [personLayer, countriesDataNew];
+}
+
+
+function removeItem(array, itemToRemove) {
+    const index = array.indexOf(itemToRemove);
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+    return array;
+}
